18986bc2bd9dfe146ee098f7f11f82b1d0232f06
juraj3-SK <juraj3@gmail.com>
2026-05-06 11:48:45 +0200

    upravena inicializacna funkcia objektu nepriatel

--- nepriatel.py
+++ nepriatel.py
@@ -3,33 +3,10 @@ from postava import postava
 import random
 
 class nepriatel(postava):
-    #nie je mi celkom jasne, ci robit novu funkciu "vytvor nahodneho", alebo len upravujem "init" - urobil som novy init
-    def __init__(self, zlato_odmena):
+    def __init__(self, id, nazov, zivoty, utok, zlato_odmena, xp_odmena):
+        super().__init__(id, nazov, zivoty, utok)
         self.zlato_odmena=zlato_odmena
-        
-        #goblin=1, ork=2, kostlivec=3, drak=4
-        temp=random.randint(1,4)
-        if (temp==1):
-            self.nazov="goblin"
-            self.zivoty=20
-            self.utok=5
-            self.xp=10
-        elif (temp==2):
-            self.nazov="ork"
-            self.zivoty=30
-            self.utok=7
-            self.xp=15
-        elif (temp==3):
-            self.nazov="kostlivec"
-            self.zivoty=25
-            self.utok=6
-            self.xp=12
-        #elif (temp==4):
-        else:
-            self.nazov="drak"
-            self.zivoty=50
-            self.utok=10
-            self.xp=30
+        self.xp_odmena=xp_odmena
 
     def func_zautoc(self, ciel):
         #generuj poskodenie
