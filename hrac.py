04b8157c5f1a520e6001442bd48ec8ba1a0914a5
juraj3-SK <juraj3@gmail.com>
2026-05-07 09:53:31 +0200

    zadefinovane nove funkcie hraca (vypis a levelovanie)

--- hrac.py
+++ hrac.py
@@ -1,24 +1,19 @@
 from postava import postava
-
 import random
+
 
 #toto nam hovori, ze class hrac dedi funkcie aj premenne od postavy
 class hrac(postava):
 
-def __init__(self, id, nazov, zivoty, utok):
-
-    def __init__(self, id, nazov, zivoty, utok, mana, level, xp, inventar, zlato):
-        
+    def __init__(self, id, nazov, zivoty, utok, mana, level=1, xp=0, inventar=[], zlato=0):
         #toto len ako reminder
         #funkcia super bere (init) od parenta
         super().__init__(id, nazov, zivoty, utok)
+
         self.mana=mana
-        
-        #tu by som chcel mat toto: Ak level bol poslany TAK ho nastav, inak 1
-        #zatial natvrdo kodujem prvu uroven
-        self.level=1
-        self.xp=0
-        self.inventar=[]
+        self.level=level
+        self.xp=xp
+        self.inventar=inventar
         self.zlato=zlato
 
     def func_liecenie(self):
@@ -30,20 +25,36 @@ def __init__(self, id, nazov, zivoty, utok):
             print("")
         else:
             print("Nemas dost many na liecenie")
-    
-    def func_vypis_stav(self):
-        print(f"Volas sa {self.nazov}, a.k.a. id={self.id}. Tvoje parametre su (zivoty / utok / mana / level / xp / zlato): {self.zivoty}, {self.utok}, {self.mana}, {self.xp}, {self.zlato}.")
-        print("")
-        if (len(inventar)==0):
-            print("Nemas so sebou ziadne predmety.")
-        elif(len(inventar)==1):
-            print("Mas so sebou tento predmet:")
-            print(inventar[0])
-        else:
-            print("Mas so sebou tieto predmety:")
-            for i in inventar:
-                print(i)
-    
-              
 
-            
+    def func_pridaj_xp(self, nepriatel):
+        self.xp+=nepriatel.xp_odmena
+        if (self.xp>=20*self.level):
+            self.xp-=20*self.level
+            self.level+=1
+            self.zivoty+=10
+            self.utok+=2
+            self.mana+=5
+
+    #ak dam print hraca, tak sa vola toto
+    def __str__(self):
+        text=""
+        text+=(f"Volas sa {self.nazov}, a.k.a. id={self.id}. Tvoje parametre su:")
+        text+=("\n")
+        text+=(f"zivoty: {self.zivoty}")
+        text+=(f"\nutok: {self.utok}")
+        text+=(f"\nmana: {self.mana}")
+        text+=(f"\nlevel: {self.level}")
+        text+=(f"\nxp: {self.xp}")
+        text+=(f"\nzlato: {self.zlato}")
+        text+=("\n")
+        if (len(self.inventar)==0):
+            text+=("\nNemas so sebou ziadne predmety.")
+        elif(len(self.inventar)==1):
+            text+=("\nMas so sebou tento predmet:")
+            text+=(f"\n{self.inventar[0]}")
+        else:
+            text+=("\nMas so sebou tieto predmety:")
+            for i in self.inventar:
+                text+=(f"\n{i}")
+        
+        return text
