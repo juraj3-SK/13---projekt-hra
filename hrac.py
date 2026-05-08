939a4d9a6121852f0da95a46c00175f363749c23
juraj3-SK <juraj3@gmail.com>
2026-05-08 08:36:57 +0200

    changes in function __str__ and in func_healing

--- hrac.py
+++ hrac.py
@@ -5,10 +5,10 @@ import random
 #toto nam hovori, ze class hrac dedi funkcie aj premenne od postavy
 class hrac(postava):
 
-    def __init__(self, id, nazov, zivoty, utok, mana, level=1, xp=0, inventar=[], zlato=0):
+    def __init__(self, id, nazov, max_zivoty, utok, mana, level=1, xp=0, inventar=[], zlato=0):
         #toto len ako reminder
         #funkcia super bere (init) od parenta
-        super().__init__(id, nazov, zivoty, utok)
+        super().__init__(id, nazov, max_zivoty, utok)
 
         self.mana=mana
         self.level=level
@@ -20,6 +20,8 @@ class hrac(postava):
         if(self.mana>=10):
             self.mana-=10
             liecenie=4+random.randint(1,6)
+            if (self.zivoty+liecenie>self.max_zivoty):
+                liecenie=self.max_zivoty-self.zivoty
             self.zivoty+=liecenie
             print(f"Pridal si si zivoty: {liecenie}. Tvoj pocet zivotov je {self.zivoty}. Mnozstvo many, ktora Ti zostala je: {self.mana}.")
             print("")
@@ -31,7 +33,7 @@ class hrac(postava):
         if (self.xp>=20*self.level):
             self.xp-=20*self.level
             self.level+=1
-            self.zivoty+=10
+            self.max_zivoty+=10
             self.utok+=2
             self.mana+=5
 
@@ -41,12 +43,28 @@ class hrac(postava):
         text+=(f"Volas sa {self.nazov}, a.k.a. id={self.id}. Tvoje parametre su:")
         text+=("\n")
         text+=(f"zivoty: {self.zivoty}")
+        text+=(f"\nmax. zivoty: {self.max_zivoty}")
         text+=(f"\nutok: {self.utok}")
         text+=(f"\nmana: {self.mana}")
         text+=(f"\nlevel: {self.level}")
         text+=(f"\nxp: {self.xp}")
         text+=(f"\nzlato: {self.zlato}")
         text+=("\n")
+        
+        # if (len(self.inventar)==0):
+        #     text+=("\nNemas so sebou ziadne predmety.")
+        # elif(len(self.inventar)==1):
+        #     text+=("\nMas so sebou tento predmet:")
+        #     text+=(f"\n{self.inventar[0]}")
+        # else:
+        #     text+=("\nMas so sebou tieto predmety:")
+        #     for i in self.inventar:
+        #         text+=(f"\n{i}")
+        text+=self.func_vypis_inventar()
+        return text
+
+    def func_vypis_inventar(self):
+        text=""
         if (len(self.inventar)==0):
             text+=("\nNemas so sebou ziadne predmety.")
         elif(len(self.inventar)==1):
@@ -56,5 +74,12 @@ class hrac(postava):
             text+=("\nMas so sebou tieto predmety:")
             for i in self.inventar:
                 text+=(f"\n{i}")
-        
         return text
+
+    def func_pridaj_predmet(self, predmet):
+        self.inventar.append(predmet)
+    
+    def func_pouzi_predmet(self, idx):
+        #consumables will be removed (when used)
+        if (self.inventar(idx).consumable==1):
+            self.inventar.pop(idx)
