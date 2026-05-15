cd297c9086944e053a7bcbbec13ab893a0a5e7c1
juraj3-SK <juraj3@gmail.com>
2026-05-15 20:10:15 +0200

    opravena funkcia init (premenna zivoty vs max_zivoty)

--- nepriatelia/drak.py
+++ nepriatelia/drak.py
@@ -2,4 +2,4 @@ from nepriatel import nepriatel
 
 class drak(nepriatel):
     def __init__(self, id):
-        super().__init__(id, nazov="drak", zivoty=50, utok=10, iniciativa=5, xp_odmena=30, zlato_odmena=30)
+        super().__init__(id, nazov="drak", max_zivoty=50, utok=10, iniciativa=5, xp_odmena=30, zlato_odmena=30)
--- nepriatelia/kostlivec.py
+++ nepriatelia/kostlivec.py
@@ -2,5 +2,4 @@ from nepriatel import nepriatel
 
 class kostlivec(nepriatel):
     def __init__(self, id):
-        super().__init__(id, nazov="kostlivec", zivoty=25, utok=6, iniciativa=10, xp_odmena=12, zlato_odmena=12)
-    
+        super().__init__(id, nazov="kostlivec", max_zivoty=25, utok=6, iniciativa=10, xp_odmena=12, zlato_odmena=12)
