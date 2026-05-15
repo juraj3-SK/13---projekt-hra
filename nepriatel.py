d1dea8bc660d06cc25bfcfa3213b3d6020932487
juraj3-SK <juraj3@gmail.com>
2026-05-15 17:51:33 +0200

    do nepriatela vraciam funkciu zautoc (zmazana v postave)

--- nepriatel.py
+++ nepriatel.py
@@ -8,9 +8,20 @@ class nepriatel(postava):
         self.zlato_odmena=zlato_odmena
         self.xp_odmena=xp_odmena
 
-    # def func_zautoc(self, ciel):
-    #     #generuj poskodenie
-    #     poskodenie=self.utok+random.randint(3,8)
-    #     print(f"Utok nepriatela {self.nazov}, a.k.a. id={self.id}, je {poskodenie}.")
-    #     ciel.func_zranenie(poskodenie)
-    #
+    def func_zautoc(self, ciel):
+        #ak by som potreboval volat funkciu rodica, tak to dam sem
+        #super().func_zautoc(ciel)
+        
+        #generuj poskodenie
+        poskodenie=self.utok+random.randint(3,8)
+
+        # Kritický zásah má 10 % šancu a dáva dvojnásobné poškodenie.
+        tempVar=random.randint(1,10)
+        print(f"vygenerovana pravdepodobnost sa rovna: {tempVar}")
+        if (tempVar==1):
+            print("Nasleduje kriticky zasah.")
+            poskodenie=2*poskodenie
+
+        #vypis info a zautoc
+        print(f"Utok nepriatela {self.nazov}, a.k.a. id={self.id}, je {poskodenie}.")
+        ciel.func_zranenie(poskodenie)
